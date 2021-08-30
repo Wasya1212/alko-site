ae51402696977d65445767f46092655a43a3c8d4
wasya1212 <wasya1212cool@gmail.com>
2021-08-31 01:55:05 +0300

    added homepage pug

--- utils/render-page.js
+++ utils/render-page.js
@@ -1,6 +1,9 @@
+const path = require('path');
 const pug = require('pug');
 
-const PUG_OPTIONS = {};
+const PUG_OPTIONS = {
+  filename: path.resolve(__dirname, '../views/views')
+};
 
 const renderPage = (page, locals) => {
   return pug.compile(page, PUG_OPTIONS)(locals || {});
